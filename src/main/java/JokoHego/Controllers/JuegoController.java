be071cbcb6da352871a9ea01dbbe14accef252a2
Sergio Garcia <sergiogarciadelgado25@gmail.com>
2020-01-24 09:39:34 +0000

    ViewEdit

--- src/main/java/JokoHego/Controllers/JuegoController.java
+++ src/main/java/JokoHego/Controllers/JuegoController.java
@@ -30,5 +30,7 @@ public class JuegoController implements Initializable {
 		// TODO Auto-generated method stub
 
 	}
+	
+	
 
 }
---
+++ src/main/java/JokoHego/Utils/Utils.java
@@ -0,0 +1,7 @@
+package JokoHego.Utils;
+
+public class Utils {
+	
+	
+
+}
