7ca1f7cc87ce03145eb085251760465a5d0fbecb
AmaroYanes <ihvamaroyanes@gmail.com>
2020-01-24 11:08:46 +0000

    Utils commit

--- src/main/java/dad/jokohego/utils/JokoUtils.java
+++ src/main/java/dad/jokohego/utils/JokoUtils.java
@@ -1,8 +1,32 @@
 package dad.jokohego.utils;
 
+import java.awt.Point;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class JokoUtils {
 	
-//	public void generarNivel(Button[][] button, BackType[][] BackType) {
-//		
-//	}
+	public GridPane generarNivel(int nivel) {
+		int size = nivel + 4;
+		Button[][] button = new Button[size+1][size+1];
+		BackType[][] backType = new BackType[size+1][size+1];;
+		GridPane buttons = new GridPane();
+		for(int i = 0;i < size;i++ ) {
+			for(int j = 0; j < size;j++) {
+				
+				button[i][j] = new Button();
+				button[i][j].setUserData(new Point(i,j));
+				buttons.add(button[i][j],i,j);
+				button[i][j].setMaxWidth(Integer.MAX_VALUE);
+				button[i][j].setMaxHeight(Integer.MAX_VALUE);
+				backType[i][j] = BackType.LosaOscura;
+				
+				
+			}
+		}
+		
+		
+		return buttons;
+	}
 }
