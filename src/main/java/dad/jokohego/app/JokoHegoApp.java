04b81cedb2aa2352f416f23a797890fe5bdd2a10
Sergio Garcia <sergiogarciadelgado25@gmail.com>
2020-02-10 12:43:03 +0000

    general commit

--- src/main/java/dad/jokohego/app/JokoHegoApp.java
+++ src/main/java/dad/jokohego/app/JokoHegoApp.java
@@ -13,7 +13,7 @@ public class JokoHegoApp extends Application {
 		
 		MenuController juego = new MenuController();
 		
-		Scene scene = new Scene(juego.getView(),750,600);
+		Scene scene = new Scene(juego.getView(),900,700);
 		primaryStage.setResizable(false);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("JokoHego");
